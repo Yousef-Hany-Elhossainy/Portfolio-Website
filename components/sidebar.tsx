9a11f19097e2111c0b5ac38fff32a6cde43c67bb
Yousef Hany <yousefhany@protonmail.com>
2021-04-03 23:44:50 +0200

    First Commit

---
+++ components/sidebar.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const Sidebar = () => {
+    return (
+        <div>
+            <img src="https://badoystudio.com/wp-content/uploads/2018/11/apa-itu-programmer.jpeg" alt="My Avatar" />
+            <h3>
+            <span>
+                Yousef 
+            </span>
+                Elhossainy
+            </h3>
+            <p>Web Developer</p>
+            <p>Download Resume</p>
+            <div>
+
+                <span>Cairo,Egypt</span>
+            </div>
+            <p>yousefhany@pm.me</p>
+            <button>Email Me</button>
+            <button>Toggle Theme</button>
+        </div>
+    )
+}
+
+export default Sidebar
