06ef49d30d1f090519edb3d9840496b74c529b24
Yousef Hany <yousefhany@protonmail.com>
2021-04-04 23:16:39 +0200

    Added Dark mode

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -2,8 +2,14 @@ import React from 'react'
 import {AiFillGithub,AiFillLinkedin} from"react-icons/ai";
 import {GoLocation} from"react-icons/go";
 import {GiTie} from"react-icons/gi";
+import {useTheme} from"next-themes";
 
 const Sidebar = () => {
+    const{ theme,setTheme } = useTheme();
+    const changeTheme = () => {
+        setTheme(theme === "light" ? "dark" : "light");
+    };
+    
     return (
         <div>
             <img src="https://badoystudio.com/wp-content/uploads/2018/11/apa-itu-programmer.jpeg" alt="My Avatar" 
@@ -12,8 +18,12 @@ const Sidebar = () => {
             <span className="text-green-400 ">Yousef </span>
                 Elhossainy
             </h3>
-            <p className="px-2 py-1 my-3 bg-gray-200 rounded-full">Web Developer</p>
-            <a className="flex items-center justify-center px-2 py-1 my-3 bg-gray-200 rounded-full" href ="" download="">
+            <p className="px-2 py-1 my-3 bg-gray-200 rounded-full dark:bg-dark-200 dark:bg-black-500">
+                Web Developer
+            </p>
+            <a className="flex items-center justify-center px-2 py-1 my-2 bg-gray-200 rounded-full cursor-pointer dark:bg-dark-200 dark:bg-black-500" 
+                href ="" 
+                download="">
                 <GiTie className="w-6 h-6"/>Download Resume</a>
             <div className="flex justify-around w-9/12 mx-auto my-5 text-blue-700 md:w-full">
                 <a href="">
@@ -26,7 +36,10 @@ const Sidebar = () => {
                     <AiFillGithub className="w-8 h-8 cursor-pointer"/>
                 </a>
             </div>
-            <div className="py-4 my-5 bg-gray-200 " style = {{marginLeft:"-1rem", marginRight:"-1rem"}}>
+            <div 
+            className="py-4 my-5 bg-gray-200 dark:bg-dark-200 " 
+                style = {{marginLeft:"-1rem", marginRight:"-1rem"}}
+            >
                <div className="flex items-center justify-center space-x-2">
                 <GoLocation />
                 <span>Cairo,Egypt</span>
@@ -36,7 +49,11 @@ const Sidebar = () => {
             
             <button className="w-8/12 px-5 py-2 my-2 rounded-full text-green bg-gradient-to-r from-purple-800 to-blue-700 focus:outline-none"
             onClick={() =>window.open('mailto:yousefhany@pm.me')} >Email Me</button>
-            <button className="w-8/12 px-5 py-2 my-2 rounded-full text-green bg-gradient-to-r from-purple-800 to-blue-700 focus:outline-none">Toggle Theme</button>
+            <button 
+                onClick= {changeTheme}
+                className="w-8/12 px-5 py-2 my-2 rounded-full text-green bg-gradient-to-r from-purple-800 to-blue-700 focus:outline-none">
+                Toggle Theme
+            </button>
         </div>
     )
 }
--- type.ts
+++ type.ts
@@ -13,3 +13,15 @@ export interface ISkill{
     level: string;
        
 }
+
+export interface Iproject{
+    name: string;
+    description: string;
+    image_path: string;
+    deployed_url: string;
+    github_url: string;
+    category: Category[];
+    key_techs: string[];
+}
+
+export type Category = "React"|"Node"|"Express"|"Mongo"|"Python"|"Solidity"|"Flask"|"C++"|"Unreal"|"TypeScript"
